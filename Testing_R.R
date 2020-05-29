bf433a58e46615476d5875f0417fd44df1d2c993
Bad_Shah <adilred7@gmail.com>
2020-05-30 05:56:00 +0600

    Did some code using tidyverse library and mpg datas.

--- Testing_R.R
+++ Testing_R.R
@@ -4,3 +4,14 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 
+library(tidyverse)
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+dim(mtcars)
+head(mpg, 1)
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy)) + facet_wrap(~ class, nrow = 2)
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy)) + facet_grid(drv ~ cyl)
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = drv, y = cyl))
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
