0c2b5f473cacbdac468ddda6dfefafeabdf59297
Bad_Shah <adilred7@gmail.com>
2020-05-30 06:02:49 +0600

    Nothing New

--- Testing_R.R
+++ Testing_R.R
@@ -15,3 +15,4 @@ ggplot(data = mpg) +
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_grid(drv ~ .)
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy)) + facet_wrap(~ class, nrow = 2)
